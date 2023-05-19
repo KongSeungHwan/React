6bfaac9fa6546404f669d53de52dd081836d6eb6
DESKTOP-U7PDAB6\Kosmo <tmdghks3520@nate.com>
2023-05-19 18:33:31 +0900

    2023.05.19 react 수업 자료

---
+++ reactstudy/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          수정하라는데<code>src/App.js</code> and 저장하라네 to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
